b619595aa0858500e862d73b5cb77ef493a453e7
Agustin Amenabar <agustin.amenabar@eqs.com>
2018-08-11 00:47:04 +0200

    Added some tests to the GET api/media route

--- tests/spec.js
+++ tests/spec.js
@@ -7,7 +7,8 @@ const fse = require('fs-extra');
 
 const UPLOADS_URL = './uploads';
 const MOCKS_DIR = './tests/mocks';
-describe(' ', () => {
+
+before(() => {
     console.log('Setup testing environment.');
     if (!stat.isDirectorySync(UPLOADS_URL)) {
         console.log('Creating uploads dir');
@@ -21,25 +22,73 @@ describe(' ', () => {
 
 // TODO: setup tests on filesystem to not depend on having files, also add cleanup later.
 describe('GET api/media', () => {
-    it('should return a folder`s content listed', done => {
+    it('should return the root folder´s content listed, with the mocks directory at least.', done => {
         request(app)
             .get('/api/media/')
             .set('Accept', 'application/json')
             .expect('Content-type', 'application/json; charset=utf-8')
             .expect(200)
             .end((err, res) => {
-                console.log('getting response');
                 if (err) return done(err);
-                expect(res.body).to.be.an('array');
+                expect(res.body).to.be.an('array').that.is.not.empty;
+                expect(res.body.find(item => item.name === 'mocks')).to.have.property(
+                    'type',
+                    'DIRECTORY'
+                );
+                done();
+            });
+    });
+    it('should list the images inside the "mocks" folder', done => {
+        request(app)
+            .get('/api/media/mocks/')
+            .set('Accept', 'application/json')
+            .expect('Content-type', 'application/json; charset=utf-8')
+            .expect(200)
+            .end((err, res) => {
+                console.log('List response', res.body);
+                if (err) return done(err);
+                expect(res.body)
+                    .to.be.an('array')
+                    .to.have.lengthOf(7); // update this when files or folders are added to the mosks folder
+                expect(
+                    // here we check for the list to have images
+                    res.body.filter(item => item.type.substr(0, 6) === 'image/')
+                ).to.have.lengthOf(5);
+                done();
+            });
+    });
+    it('should return an empty array when there are no images in the dir.', done => {
+        request(app)
+            .get('/api/media/mocks/empty/')
+            .set('Accept', 'application/json')
+            .expect('Content-type', 'application/json; charset=utf-8')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body)
+                    .to.be.an('array')
+                    .to.have.lengthOf(0);
+                done();
+            });
+    });
+    it('should return the contents of sub folders.', done => {
+        request(app)
+            .get('/api/media/mocks/venice/')
+            .set('Accept', 'application/json')
+            .expect('Content-type', 'application/json; charset=utf-8')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body)
+                    .to.be.an('array')
+                    .to.have.lengthOf(2); // this must be updated in the case of adding files to venice folder
                 done();
             });
     });
 });
 
-describe('Teardown of testing environment', () => {
+after(() => {
+    console.log('Teardown of testing environment');
     // remove mock files from testing env.
     fse.removeSync(`${UPLOADS_URL}/mocks`);
-    it('done teardown', done => {
-        done();
-    });
 });
