3b2a6ef78ce2e967735c20a9c670272c8e6734e5
B. Agustin Amenabar L <baamenabar@gmail.com>
2019-02-12 23:43:30 +0100

    Reconfiguring prettier and eslint

--- server/mimetypes.js
+++ server/mimetypes.js
@@ -13,27 +13,27 @@ const uploadableTypes = {
  * This returns an extension for a given filetype, if it's on the list of accepted types
  * @param {string} mimetype
  */
-const getExtensionFromAcceptedMimeType = mimetype => {
+const getExtensionFromAcceptedMimeType = (mimetype) => {
     const extension = Object.getOwnPropertyNames(uploadableTypes).filter(
         type => uploadableTypes[type] === mimetype
     );
-    //console.log("mime-type:", mimetype, extension);
+    // console.log("mime-type:", mimetype, extension);
     return extension.length ? extension[0] : '';
 };
 
-const isListableFileType = path => {
+const isListableFileType = (path) => {
     const result = !!getExtensionFromAcceptedMimeType(stats.getMimeTypeAndExtension(path).mimetype);
 
     return !!result;
-}
+};
 
-const isListableMimeType = mimetype => {
-    return Object.getOwnPropertyNames(uploadableTypes).some(extension => uploadableTypes[extension] === mimetype);
-}
+const isListableMimeType = mimetype => Object.getOwnPropertyNames(uploadableTypes).some(
+    extension => uploadableTypes[extension] === mimetype
+);
 
 module.exports = {
     getExtensionFromAcceptedMimeType,
     isListableFileType,
     isListableMimeType,
     uploadableTypes,
-}
+};
